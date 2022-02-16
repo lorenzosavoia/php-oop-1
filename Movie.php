0760d3bb980a8e48fad25592ad6fc01deb1d6fcb
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-02-16 20:09:43 +0100

    end

--- Movie.php
+++ Movie.php
@@ -9,48 +9,83 @@ class Movie
     public $actor;
     public $language;
 
-    public function __construct($title,$price,$language)
+    public function __construct($title, $price, $language)
     {
         $this->title = $title;
         $this->price = $price;
-        $this->$language = $language;
+        $this->language = $language;
     }
 
-    public function setTitle($title){
+    public function setTitle($title)
+    {
         $this->title = $title;
     }
     public function getTitle()
     {
         return $this->title;
-    }  
-    public function setPrice($price){
+    }
+    public function setPrice($price)
+    {
         $this->price = $price;
     }
-    public function getPrice(){
+    public function getPrice()
+    {
         return $this->price;
     }
-    public function setDuration($duration){
+    public function setDuration($duration)
+    {
         $this->duration = $duration;
     }
-    public function getDuration(){
+    public function getDuration()
+    {
         return $this->duration;
     }
-    public function setGenre($genre){
+    public function setGenre($genre)
+    {
         $this->genre = $genre;
     }
-    public function getGenre(){
+    public function getGenre()
+    {
         return $this->genre;
     }
-    public function setActor($actor){
-        $this->actore = $actor;
+    public function setActor($actor)
+    {
+        $this->actor = $actor;
     }
-    public function getActor(){
+    public function getActor()
+    {
         return $this->actor;
     }
-    public function setLanguage($language){
+    public function setLanguage($language)
+    {
         $this->language = $language;
     }
-    public function getLanguage(){
+    public function getLanguage()
+    {
         return $this->language;
     }
 }
+$movies = new Movie('Assassinio sul Nilo', 10, 'IT');
+$movies->setActor('Kenneth Branagh');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li>titolo: <?= $movies->getTitle() ?></li>
+        <li>prezzo: <?= $movies->getPrice() ?>€</li>
+        <li>lingua: <?= $movies->getLanguage() ?></li>
+        <li>Nei panni di Hercule Poirot: <?= $movies->getActor() ?></li>
+    </ul>
+</body>
+
+</html>
